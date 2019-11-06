accff6c1a6a0753b900f7ee5bb507e20534d2e0d
Sean Carey <seancarey18@gmail.com>
2019-11-05 22:18:35 -0500

    Track build and media folders

---
+++ frontend/build/root/precache-manifest.3b1f301418b1639c8bb2f1d19a564398.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6886045f7306d70af824b67ba8fa1f58",
+    "url": "./static/media/wsie_logo.6886045f.png"
+  },
+  {
+    "revision": "99770d9a73cfa575bcfecaf1328b5b15",
+    "url": "./static/media/sad-plate.99770d9a.png"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "9b22fd0c8cbab6505535",
+    "url": "./static/js/main.9b22fd0c.chunk.js"
+  },
+  {
+    "revision": "19d5d187d81c53987aa3",
+    "url": "./static/js/2.19d5d187.chunk.js"
+  },
+  {
+    "revision": "9b22fd0c8cbab6505535",
+    "url": "./static/css/main.c427cd23.chunk.css"
+  },
+  {
+    "revision": "0d5d4b8a22acf6d3a1467b66fdba6dd3",
+    "url": "./index.html"
+  }
+];
