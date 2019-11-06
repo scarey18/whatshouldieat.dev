062e17bb95cb155687a69f53ce85cc04133185b6
Sean Carey <seancarey18@gmail.com>
2019-11-05 20:33:18 -0500

    Add https settings to production build

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -6,10 +6,18 @@ DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS += ['www.whatshouldieat.dev', os.environ.get('HOST', '')]
+ALLOWED_HOSTS += ['www.whatshouldieat.dev']
 
 TEMPLATES[0]['DIRS'] += [os.path.join(BASE_DIR, 'frontend/build')]
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build/static/'),
 ]
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+SECURE_HSTS_SECONDS = 3600
